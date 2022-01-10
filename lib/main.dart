0d3c800937cddd5bce2aabf4c00d7980ae1c28dc
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-10 23:42:02 +0900

    Update bottom navigators & Sync done status with db
    - Add the history page
    - Apply to move bottom navigator pages
    - Update to save done or undone status to db

--- lib/main.dart
+++ lib/main.dart
@@ -36,11 +36,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final dbHelper = DatabaseHelper.instance;
+  int selectIndex = 0;
 
   List<Todo> todos = [];
 
   void getTodayTodo() async {
     todos = await dbHelper.getTodoByDate(Utils.getFormatTime(DateTime.now()));
+    setState(() {});
+  }
+
+  void getAllTodo() async {
+    allTodo = await dbHelper.getAllTodo();
     setState(() {});
   }
 
@@ -65,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         onPressed: () async {
           // 화면 이동
-          Todo todo = await Navigator.of(context).push(
+          Todo _ = await Navigator.of(context).push(
             MaterialPageRoute(
               builder: (BuildContext ctx) => TodoWritePage(
                 todo: Todo(
@@ -83,98 +89,138 @@ class _MyHomePageState extends State<MyHomePage> {
           getTodayTodo();
         },
       ),
-      body: ListView.builder(
-        // builder 를 사용할 때 if statement 로 return 을 하게 된다면,
-        // itemCount 개수에 맞게 혹은 마지막 조건은 else 로 표시할 것 -> null 반환이 예상될 경우 app build 가 되지 않음
-        itemCount: 4,
-        itemBuilder: (BuildContext ctx, int idx) {
-          if (idx == 0) {
-            return Container(
-              child: const Text(
-                "오늘 하루",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 20), // 고정값은 항상 const 를 사용하는 것을 권장
-            );
-          } else if (idx == 1) {
-            List<Todo> undone = todos.where((t) {
-              return t.done == 0;
-            }).toList();
-
-            return Column(
-              children: List.generate(
-                undone.length,
-                (_idx) {
-                  Todo t = undone[_idx];
-                  return InkWell(
-                    child: TodoCardWidget(t: t),
-                    onTap: () {
-                      setState(() {
-                        if (t.done == 0) {
-                          t.done = 1;
-                        } else {
-                          t.done = 0;
-                        }
-                      });
-                    },
-                    onLongPress: () async {
-                      getTodayTodo();
-                    },
-                  );
-                },
-              ),
-            );
-          } else if (idx == 2) {
-            return Container(
-              child: const Text(
-                "완료된 하루",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 20), // 고정값은 항상 const 를 사용하는 것을 권장
-            );
-          } else if (idx == 3) {
-            List<Todo> done = todos.where((t) {
-              return t.done == 1;
-            }).toList();
-
-            return Column(
-              children: List.generate(
-                done.length,
-                (_idx) {
-                  Todo t = done[_idx];
-                  return InkWell(
-                    child: TodoCardWidget(t: t),
-                    onTap: () {
-                      setState(() {
-                        if (t.done == 0) {
-                          t.done = 1;
-                        } else {
-                          t.done = 0;
-                        }
-                      });
-                    },
-                    onLongPress: () async {
-                      // 수정
-                      Todo _ = await Navigator.of(context).push(
-                        MaterialPageRoute(builder: (BuildContext ctx) => TodoWritePage(todo: t)),
-                      );
-                      setState(() {});
-                    },
-                  );
-                },
-              ),
-            );
-          }
-          return Container();
-        },
-      ),
+      body: getPage(),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.today_outlined), label: "오늘"),
           BottomNavigationBarItem(icon: Icon(Icons.assignment_outlined), label: "기록"),
           BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: "더보기"),
         ],
+        currentIndex: selectIndex,
+        onTap: (idx) {
+          if (idx == 1) {
+            getAllTodo();
+          }
+          setState(() {
+            selectIndex = idx;
+          });
+        },
       ),
+    );
+  }
+
+  Widget getPage() {
+    if (selectIndex == 0) {
+      return getMain();
+    } else {
+      return getHistory();
+    }
+  }
+
+  Widget getMain() {
+    return ListView.builder(
+      // builder 를 사용할 때 if statement 로 return 을 하게 된다면,
+      // itemCount 개수에 맞게 혹은 마지막 조건은 else 로 표시할 것 -> null 반환이 예상될 경우 app build 가 되지 않음
+      itemCount: 4,
+      itemBuilder: (BuildContext ctx, int idx) {
+        if (idx == 0) {
+          return Container(
+            child: const Text(
+              "오늘 하루",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 20), // 고정값은 항상 const 를 사용하는 것을 권장
+          );
+        } else if (idx == 1) {
+          List<Todo> undone = todos.where((t) {
+            return t.done == 0;
+          }).toList();
+
+          return Column(
+            children: List.generate(
+              undone.length,
+              (_idx) {
+                Todo t = undone[_idx];
+                return InkWell(
+                  child: TodoCardWidget(t: t),
+                  onTap: () async {
+                    setState(() {
+                      if (t.done == 0) {
+                        t.done = 1;
+                      } else {
+                        t.done = 0;
+                      }
+                    });
+                    // Update card status (Done | Undone)
+                    await dbHelper.insertTodo(t);
+                  },
+                  onLongPress: () async {
+                    // 수정
+                    Todo _ = await Navigator.of(context).push(
+                      MaterialPageRoute(builder: (BuildContext ctx) => TodoWritePage(todo: t)),
+                    );
+                    setState(() {});
+                  },
+                );
+              },
+            ),
+          );
+        } else if (idx == 2) {
+          return Container(
+            child: const Text(
+              "완료된 하루",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 20), // 고정값은 항상 const 를 사용하는 것을 권장
+          );
+        } else if (idx == 3) {
+          List<Todo> done = todos.where((t) {
+            return t.done == 1;
+          }).toList();
+
+          return Column(
+            children: List.generate(
+              done.length,
+              (_idx) {
+                Todo t = done[_idx];
+                return InkWell(
+                  child: TodoCardWidget(t: t),
+                  onTap: () async {
+                    setState(() {
+                      if (t.done == 0) {
+                        t.done = 1;
+                      } else {
+                        t.done = 0;
+                      }
+                    });
+                    // Update card status (Done | Undone)
+                    await dbHelper.insertTodo(t);
+                  },
+                  onLongPress: () async {
+                    // 수정
+                    Todo _ = await Navigator.of(context).push(
+                      MaterialPageRoute(builder: (BuildContext ctx) => TodoWritePage(todo: t)),
+                    );
+                    setState(() {});
+                  },
+                );
+              },
+            ),
+          );
+        }
+        return Container();
+      },
+    );
+  }
+
+  List<Todo> allTodo = [];
+
+  Widget getHistory() {
+    return ListView.builder(
+      itemCount: allTodo.length,
+      itemBuilder: (BuildContext ctx, int idx) {
+        return TodoCardWidget(t: allTodo[idx]);
+      },
     );
   }
 }
@@ -186,6 +232,9 @@ class TodoCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int now = Utils.getFormatTime(DateTime.now());
+    DateTime time = Utils.numToDateTime(t.date);
+
     return Container(
       decoration: BoxDecoration(color: Color(t.color), borderRadius: BorderRadius.circular(16)),
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12), // Container 내의 간격
@@ -207,7 +256,13 @@ class TodoCardWidget extends StatelessWidget {
           Text(
             t.memo,
             style: const TextStyle(color: Colors.white),
-          )
+          ),
+          now == t.date
+              ? Container()
+              : Text(
+                  "${time.month}월 ${time.day}일",
+                  style: const TextStyle(color: Colors.white),
+                ),
         ],
       ),
     );
