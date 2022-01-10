b292f70504843a70ba2a2c8196d7140741697c6b
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-10 19:52:46 +0900

    Add to write todo page & arange code indentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo_flutter/write.dart';
 
 import 'data/todo.dart';
+import 'data/util.dart';
 
 void main() {
   runApp(const MyApp());
@@ -59,8 +61,35 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: PreferredSize(
         // AppBar 를 활용하여 상단의 시간, 배터리 등의 라인 아래부터 시작할 수 있음
-        child: AppBar(),
         preferredSize: const Size.fromHeight(0),
+        child: AppBar(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () async {
+          // 화면 이동
+          Todo todo = await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext ctx) => TodoWritePage(
+                todo: Todo(
+                  title: "",
+                  color: 0,
+                  memo: "",
+                  done: 0,
+                  category: "",
+                  date: Utils.getFormatTime(DateTime.now()),
+                ),
+              ),
+            ),
+          );
+
+          setState(() {
+            todos.add(todo);
+          });
+        },
       ),
       body: ListView.builder(
         // builder 를 사용할 때 if statement 로 return 을 하게 된다면,
@@ -69,7 +98,10 @@ class _MyHomePageState extends State<MyHomePage> {
         itemBuilder: (BuildContext ctx, int idx) {
           if (idx == 0) {
             return Container(
-              child: const Text("오늘하루", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              child: const Text(
+                "오늘 하루",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
               margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 20), // 고정값은 항상 const 를 사용하는 것을 권장
             );
           } else if (idx == 1) {
@@ -78,68 +110,93 @@ class _MyHomePageState extends State<MyHomePage> {
             }).toList();
 
             return Column(
-                children: List.generate(undone.length, (_idx) {
-              Todo t = undone[_idx];
-              return Container(
-                  decoration: BoxDecoration(color: Color(t.color), borderRadius: BorderRadius.circular(16)),
-                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12), // Container 내의 간격
-                  margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20), // Container 간의 간격
-                  child: Column(
-                    // 왼쪽 정렬 (default: center)
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(t.title,
-                              style: const TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold)),
-                          Text(t.done == 0 ? "미완료" : "완료", style: const TextStyle(color: Colors.white)),
-                        ],
-                      ),
-                      Container(
-                        height: 8,
-                      ),
-                      Text(t.memo, style: const TextStyle(color: Colors.white))
-                    ],
-                  ));
-            }));
+              children: List.generate(
+                undone.length,
+                (_idx) {
+                  Todo t = undone[_idx];
+                  return Container(
+                    decoration: BoxDecoration(color: Color(t.color), borderRadius: BorderRadius.circular(16)),
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12), // Container 내의 간격
+                    margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20), // Container 간의 간격
+                    child: Column(
+                      // 왼쪽 정렬 (default: center)
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(t.title,
+                                style: const TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold)),
+                            Text(t.done == 0 ? "미완료" : "완료", style: const TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                        Container(
+                          height: 8,
+                        ),
+                        Text(
+                          t.memo,
+                          style: const TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
           } else if (idx == 2) {
             return Container(
-              child: const Text("완료된 하루", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              child: const Text(
+                "완료된 하루",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
               margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 20), // 고정값은 항상 const 를 사용하는 것을 권장
             );
-          } else {
+          } else if (idx == 3) {
             List<Todo> done = todos.where((t) {
               return t.done == 1;
             }).toList();
 
             return Column(
-                children: List.generate(done.length, (_idx) {
-              Todo t = done[_idx];
-              return Container(
-                  decoration: BoxDecoration(color: Color(t.color), borderRadius: BorderRadius.circular(16)),
-                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12), // Container 내의 간격
-                  margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20), // Container 간의 간격
-                  child: Column(
-                    // 왼쪽 정렬 (default: center)
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(t.title,
-                              style: const TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold)),
-                          Text(t.done == 0 ? "미완료" : "완료", style: const TextStyle(color: Colors.white)),
-                        ],
-                      ),
-                      Container(
-                        height: 8,
-                      ),
-                      Text(t.memo, style: const TextStyle(color: Colors.white))
-                    ],
-                  ));
-            }));
+              children: List.generate(
+                done.length,
+                (_idx) {
+                  Todo t = done[_idx];
+                  return Container(
+                    decoration: BoxDecoration(color: Color(t.color), borderRadius: BorderRadius.circular(16)),
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12), // Container 내의 간격
+                    margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20), // Container 간의 간격
+                    child: Column(
+                      // 왼쪽 정렬 (default: center)
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              t.title,
+                              style: const TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              t.done == 0 ? "미완료" : "완료",
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          height: 8,
+                        ),
+                        Text(
+                          t.memo,
+                          style: const TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
           }
+          return Container();
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
